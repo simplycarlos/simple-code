0438f8c8e992c67132376e701076ba24f0cb1949
carlospinto <carlospinto@192.168.2.174>
2017-01-26 21:58:42 -0500

    bubble sort

--- src/sorting/BubbleSort.java
+++ src/sorting/BubbleSort.java
@@ -1,10 +1,32 @@
 package sorting;
 
 public class BubbleSort {
+	
+	public static void sort(int[] a){
+		// like a bubble it rises to the top
+		// compares it with all bigger indices
+		for (int i=0;i<a.length;i++){
+			int j=i;;
+			while(j<(a.length-1)){
+				if(a[j]>a[j+1]){
+					int t = a[j+1];
+					a[j+1] = a[j];
+					a[j] = t;
+				}
+				j++;
+			}
+		}
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] n = {0,4,6,2,4,6};
+		for (int i:n)
+			System.out.print(i);
+		
+		System.out.print("\n");
+		sort(n);
+		for (int i:n)
+			System.out.print(i);
 	}
 
 }
